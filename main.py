e940ec98ff9f06bb0823a95597bf55bfc1da4f9c
emailmeydrive <emailme.ydrive@gmail.com>
2022-01-19 14:40:19 +0200

    movements

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import time
 
 # screen size 
 WINDOW_W = 1000
-WINDOW_H = 800
+WINDOW_H = 524
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 
-BK_COLOR = (68,132,88)
+# BK_COLOR = (68,132,88)
 
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("My First Game")
+pygame.display.set_caption("star wars")
 
 bk_image = pygame.image.load("background.jpg")
+ship_image=pygame.image.load("space ship.png")
+ship_image=pygame.transform.scale(ship_image,(50,80))
 
 clock = pygame.time.Clock()
 
@@ -21,9 +23,13 @@ circle_x = 10
 circle_y = WINDOW_H /2
 x_step = 10
 play = True
+ship_x=WINDOW_W/2-25
+ship_y=WINDOW_H-80
+fire_blast=ship_y-50
 while play:
   # screen.fill(BK_COLOR)
   screen.blit(bk_image,(0,0))
+  screen.blit(ship_image,(ship_x,ship_y))
   pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
   
   circle_x +=x_step
@@ -32,10 +38,22 @@ while play:
   if circle_x <0 :
     x_step = 10
   
+  
   pygame.display.flip()
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
+    elif event.type==pygame.KEYDOWN:
+      if event.key==pygame.K_LEFT:
+        ship_x-=10
+      if event.key==pygame.K_RIGHT:
+        ship_x+=10
+      if event.key==pygame.K_BACKSPACE:
+        pygame.draw.circle(screen,(255,255,255),(ship_x , fire_blast),10)
+        fire_blast-=5
+
+
+
   clock.tick(50)
 
 pygame.quit()
