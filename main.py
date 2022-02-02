6d799bb4cb5573600c56108492a77e9bafc2fdc2
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-02 14:57:28 +0200

    boom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import circle
+from pickle import TRUE
+from turtle import circle, window_height
 import pygame
 import time
 
@@ -10,14 +11,25 @@ WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 # music defenition
 pygame.mixer.init()
 pygame.mixer.music.load("song.mp3")
-pygame.mixer.music.set_volume(0.0)
+pygame.mixer.music.set_volume(0.0  )
 pygame.mixer.music.play()
 pygame.mixer.music.load("gunshot.mp3")
 pygame.mixer.music.set_volume(1.0)
-
-
+pygame.mixer.music.load("explosion.mp3")
+pygame.mixer.music.set_volume(7.0)
 pygame.mixer.Channel(1).play(pygame.mixer.Sound('song.mp3'))
-# BK_COLOR = (68,132,88)
+
+# text_color = (68,132,88)
+# myfont = pygame.FONT.skyfont("comicsans",30)
+# textsurfase=myfont.render('score:',True,(255,255,255))
+
+score1=0
+pygame.font.init()
+myfont = pygame.font.SysFont("Comic Sans MS",30)
+textsurfase=myfont.render('score:',True,(255,255,255))
+font=pygame.font.SysFont("fhir",30,50)
+score=font.render(str(score1),False,(255,255,255))
+
 
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
@@ -27,6 +39,7 @@ ship_image=pygame.image.load("space ship.png")
 laser_image=pygame.image.load("pngaaa.com-25809.png")
 ship_image=pygame.transform.scale(ship_image,(50,80))
 laser_image=pygame.transform.scale(laser_image,(30,40))
+
 clock = pygame.time.Clock()
 
 circle_x = 10
@@ -37,19 +50,30 @@ ship_x=WINDOW_W/2-25
 ship_y=WINDOW_H-80
 lasers_beam=[]
 
+def laser_hit(laser):
+  return abs(laser[0]-circle_x)<20 and abs(laser[1]-circle_y)<20 
+
 
 
 def laserbeam():
-#   pygame.draw.line(screen,(255,255,255),(0,1),(1000,1))
+  # pygame.draw.line(screen,(255,255,255),(0,1),(1000,1))
   # pygame.draw.line(screen,(255,255,255),(0,2),(1000,2))
   # pygame.draw.line(screen,(255,255,255),(0,3),(1000,3))
   # pygame.draw.line(screen,(255,255,255),(0,4),(1000,4))
   # pygame.draw.line(screen,(255,255,255),(0,5),(1000,5))
   # pygame.draw.line(screen,(255,255,255),(0,6),(1000,6)) 
-  for i in range(len(lasers_beam)):
+  for i in range(len(lasers_beam)-1,0,-1):
+    global score1
+    global circle_x 
     lb=lasers_beam[i]
     screen.blit(laser_image,(lb[0],lb[1]))
-    lasers_beam[i]=[lb[0],lb[1]-10]
+    lasers_beam[i]=[lb[0],lb[1]-5]
+    if laser_hit(lb):
+      score1+=1
+      pygame.mixer.Channel(3).play(pygame.mixer.Sound('explosion.mp3'))
+      circle_x=0
+      lasers_beam.pop(i)
+      
    
       
 
@@ -57,10 +81,13 @@ def laserbeam():
 
 while play:
   # screen.fill(BK_COLOR)
+  screen.blit(textsurfase,(10,20))
   screen.blit(bk_image,(0,0)) 
   screen.blit(ship_image,(ship_x,ship_y))
   pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
-  
+  screen.blit(textsurfase,(10,5))
+  score=font.render(str(score1),False,(255,255,255)) 
+  screen.blit(score,(100,20))
   circle_x +=x_step
   if circle_x > WINDOW_W:
     x_step = -10
@@ -79,6 +106,7 @@ while play:
       if event.key==pygame.K_SPACE:
         lasers_beam.append([laser_x,ship_y])
         pygame.mixer.Channel(0).play(pygame.mixer.Sound('gunshot.mp3'))
+        
   laserbeam()
         
    
